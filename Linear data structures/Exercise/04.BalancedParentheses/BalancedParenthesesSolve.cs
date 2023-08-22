f6d5dd9dcf8d39c61aca4d9c1651b16330db822c
Anton Todorov <98225837+Peshp@users.noreply.github.com>
2023-08-22 11:25:41 +0300

    Update BalancedParenthesesSolve.cs

--- Linear data structures/Exercise/04.BalancedParentheses/BalancedParenthesesSolve.cs
+++ Linear data structures/Exercise/04.BalancedParentheses/BalancedParenthesesSolve.cs
@@ -7,7 +7,62 @@
     {
         public bool AreBalanced(string parentheses)
         {
-            throw new NotImplementedException();
+            //First method
+
+            //int count = 0;
+            //for (int i = 0; i < parentheses.Length; i++)
+            //{
+            //    if (parentheses[i] == '{')
+            //        count++;
+            //    else if (parentheses[i] == '}')
+            //        count--;
+            //
+            //    if (parentheses[i] == '[')
+            //        count++;
+            //    else if (parentheses[i] == ']')
+            //        count--;
+            //
+            //    if (parentheses[i] == '(')
+            //        count++;
+            //    else if (parentheses[i] == ')')
+            //        count--;
+            //}
+            //
+            //if (count == 0)
+            //    return true;
+            //return false;
+
+            //Second Method
+
+            var opened = new List<char> {'(', '[', '{'};
+            var closed = new List<char> {')', ']', '}'};
+
+            var matching = new Dictionary<char, char>
+            {
+                ['{'] = '}',
+                ['['] = ']',
+                ['('] = ')',
+            };
+            var result = new Stack<char>();
+
+            foreach (char c in parentheses)
+            {
+                if(opened.Contains(c))
+                    result.Push(c);
+                else
+                {
+                    if(result.Count == 0)
+                        return false;
+                    if(closed.Contains(c))
+                    {
+                        if (matching[result.Peek()] == c)
+                            result.Pop();
+                        else
+                            return false;
+                    }
+                }
+            }
+            return true;
         }
     }
 }
