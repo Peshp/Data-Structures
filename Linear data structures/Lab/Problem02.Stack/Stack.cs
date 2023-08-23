d2960d4513c0249f15343f3eee06d1993f69a760
Anton Todorov <98225837+Peshp@users.noreply.github.com>
2023-08-23 11:16:20 +0300

    Create Stack.cs

---
+++ Linear data structures/Lab/Problem02.Stack/Stack.cs
@@ -0,0 +1,81 @@
+﻿namespace Problem02.Stack
+{
+    using System;
+    using System.Collections;
+    using System.Collections.Generic;
+
+    public class Stack<T> : IAbstractStack<T>
+    {        
+        private class Node
+        {
+            public Node(T element, Node next)
+            {
+                Element = element;
+                Next = next;
+            }
+
+            public Node(T element)
+            {
+                Element = element;
+            }
+
+            public T Element { get; set; }
+            public Node Next { get; set; }
+        }
+
+        private Node top;
+
+        public int Count { get; private set; }
+
+        public void Push(T item)
+        {
+            top = new Node(item, top);
+            Count++;
+        }
+
+        public T Pop()
+        {
+            if (Count == 0)
+                throw new InvalidOperationException();
+
+            var current = top;
+            top = current.Next;
+            Count--;
+            return current.Element;
+        }
+
+        public T Peek()
+        {
+            if (Count == 0)
+                throw new InvalidOperationException();
+
+            return top.Element;
+        }
+
+        public bool Contains(T item)
+        {
+            var node = top;
+            while (node != null)
+            {
+                if (node.Element.Equals(item))
+                    return true;
+
+                node = node.Next;
+            }
+            return false;
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            var node = top;
+            while (node != null)
+            {
+                yield return top.Element;
+                node = node.Next;                
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+            => GetEnumerator();
+    }
+}
