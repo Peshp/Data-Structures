6e04b21426b5a1f0490da3b6b0a8b4780c1be628
Anton Todorov <98225837+Peshp@users.noreply.github.com>
2023-08-26 21:27:20 +0300

    Create Program.cs

---
+++ Trees Representation and Traversal (BFS, DFS)/Lab/Tree/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Tree
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var tree = new Tree<int>(34);
+            tree.AddChild(34, new Tree<int>(20));
+            tree.AddChild(34, new Tree<int>(25));
+            tree.AddChild(34, new Tree<int>(88));
+            tree.AddChild(88, new Tree<int>(5));
+
+            Console.WriteLine(string.Join(" ", tree.OrderBfs()));
+
+            tree.Swap(88, 5);
+
+            Console.WriteLine(string.Join(" ", tree.OrderBfs()));
+        }
+    }
+}
