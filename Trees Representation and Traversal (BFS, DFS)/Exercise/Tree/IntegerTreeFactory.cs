4d09f1eb063143ed52dadb4c8dfba3c0289acfe8
Anton Todorov <98225837+Peshp@users.noreply.github.com>
2023-08-26 21:28:00 +0300

    Create IntegerTreeFactory.cs

---
+++ Trees Representation and Traversal (BFS, DFS)/Exercise/Tree/IntegerTreeFactory.cs
@@ -0,0 +1,63 @@
+﻿namespace Tree
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class IntegerTreeFactory
+    {
+        private Dictionary<int, IntegerTree> nodesByKey;
+
+        public IntegerTreeFactory()
+        {
+            this.nodesByKey = new Dictionary<int, IntegerTree>();
+        }
+
+        public IntegerTree CreateTreeFromStrings(string[] input)
+        {
+            foreach (var node in input)
+            {
+                var keys = node.ToString().Split().Select(int.Parse).ToArray();
+
+                var parent = keys[0];
+                var child = keys[1];
+
+                AddEdge(parent, child);
+            }
+
+            return GetRoot();
+        }
+
+        public IntegerTree CreateNodeByKey(int key)
+        {
+            if(!this.nodesByKey.ContainsKey(key))
+            {
+                nodesByKey.Add(key, new IntegerTree(key));
+            }
+
+            return this.nodesByKey[key];
+        }
+
+        public void AddEdge(int parent, int child)
+        {
+            var parentNode = CreateNodeByKey(parent);
+            var childNode = CreateNodeByKey(child);
+
+            parentNode.AddChild(childNode);
+            childNode.AddParent(parentNode);
+        }
+
+        public IntegerTree GetRoot()
+        {
+            foreach(var node in nodesByKey)
+            {
+                if (nodesByKey.ContainsKey(node.Key))
+                {
+                    return nodesByKey[node.Key];
+                }
+            }
+
+            return null;
+        }
+    }
+}
