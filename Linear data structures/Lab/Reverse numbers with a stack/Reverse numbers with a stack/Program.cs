c646feb7ab74350234dbd130bcf301c4fc677d13
Anton Todorov <98225837+Peshp@users.noreply.github.com>
2023-08-27 12:24:24 +0300

    Create Program.cs

---
+++ Linear data structures/Lab/Reverse numbers with a stack/Reverse numbers with a stack/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Reverse_numbers_with_a_stack
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var stack = new Stack<int>(Console.ReadLine().Split().Select(int.Parse).ToArray());
+            var result = ReverseNumbers(stack);
+
+            Console.WriteLine(string.Join(" ", result));
+        }
+
+        private static List<int> ReverseNumbers(Stack<int> numbers)
+        {
+            var result = new List<int>();
+            while (numbers.Any())
+                result.Add(numbers.Pop());
+
+            return result;
+        }
+    }
+}
