6ef1caa436ee5a4c91fd958859d85651d0f20510
Anton Todorov <98225837+Peshp@users.noreply.github.com>
2023-08-30 17:27:16 +0300

    Update BinarySearchTree.cs

--- Heaps, BST/Exercise/01.BinarySearchTree/BinarySearchTree.cs
+++ Heaps, BST/Exercise/01.BinarySearchTree/BinarySearchTree.cs
@@ -16,6 +16,8 @@
             public T Value { get; }
             public Node Left { get; set; }
             public Node Right { get; set; }
+
+            public int Count { get; set; }
         }
 
         private Node root;
@@ -33,6 +35,23 @@
         {
             this.root = this.Insert(element, this.root);
         }
+        private Node Insert(T element, Node node)
+        {
+            if (node == null)
+            {
+                node = new Node(element);
+            }
+            else if (element.CompareTo(node.Value) < 0)
+            {
+                node.Left = this.Insert(element, node.Left);
+            }
+            else if (element.CompareTo(node.Value) > 0)
+            {
+                node.Right = this.Insert(element, node.Right);
+            }
+
+            return node;
+        }
 
         public bool Contains(T element)
         {
@@ -45,12 +64,57 @@
         {
             this.EachInOrder(this.root, action);
         }
+        private void EachInOrder(Node node, Action<T> action)
+        {
+            if (node == null)
+            {
+                return;
+            }
+
+            this.EachInOrder(node.Left, action);
+            action(node.Value);
+            this.EachInOrder(node.Right, action);
+        }
 
         public IBinarySearchTree<T> Search(T element)
         {
             Node current = this.FindElement(element);
 
             return new BinarySearchTree<T>(current);
+        }
+        private Node FindElement(T element)
+        {
+            Node current = this.root;
+
+            while (current != null)
+            {
+                if (current.Value.CompareTo(element) > 0)
+                {
+                    current = current.Left;
+                }
+                else if (current.Value.CompareTo(element) < 0)
+                {
+                    current = current.Right;
+                }
+                else
+                {
+                    break;
+                }
+            }
+
+            return current;
+        }
+
+        private void PreOrderCopy(Node node)
+        {
+            if (node == null)
+            {
+                return;
+            }
+
+            this.Insert(node.Value);
+            this.PreOrderCopy(node.Left);
+            this.PreOrderCopy(node.Right);
         }
 
         public void Delete(T element)
@@ -95,96 +159,25 @@
 
         public IEnumerable<T> Range(T startRange, T endRange)
         {
-            var list = new List<T>();
+            var result = new Queue<T>();
 
-            this.GetPath(list, this.root, startRange, endRange);
+            this.Range(root, result, startRange, endRange);
 
-            return list;
+            return result;
         }
 
-        private void GetPath(List<T> list, Node node, T startRange, T endRange)
+        private void Range(Node node, Queue<T> queue, T startRange, T endRange)
         {
             if (node == null)
                 return;
 
-            if (node.Value.CompareTo(startRange) > 0)
-            {
-                this.GetPath(list, node.Left, startRange, endRange);
-            }
-            if(node.Value.CompareTo(startRange) >= 0
-                && node.Value.CompareTo(endRange) <= 0)
-            {
-                list.Add(node.Value);
-            }
-            if (node.Value.CompareTo(endRange) < 0)
-            {
-                this.GetPath(list, node.Right, startRange, endRange);
-            }
-        }
-
-        private Node FindElement(T element)
-        {
-            Node current = this.root;
-
-            while (current != null)
-            {
-                if (current.Value.CompareTo(element) > 0)
-                {
-                    current = current.Left;
-                }
-                else if (current.Value.CompareTo(element) < 0)
-                {
-                    current = current.Right;
-                }
-                else
-                {
-                    break;
-                }
-            }
-
-            return current;
-        }
-
-        private void PreOrderCopy(Node node)
-        {
-            if (node == null)
-            {
-                return;
-            }
-
-            this.Insert(node.Value);
-            this.PreOrderCopy(node.Left);
-            this.PreOrderCopy(node.Right);
-        }
-
-        private Node Insert(T element, Node node)
-        {
-            if (node == null)
-            {
-                node = new Node(element);
-            }
-            else if (element.CompareTo(node.Value) < 0)
-            {
-                node.Left = this.Insert(element, node.Left);
-            }
-            else if (element.CompareTo(node.Value) > 0)
-            {
-                node.Right = this.Insert(element, node.Right);
-            }
-
-            return node;
-        }
-
-        private void EachInOrder(Node node, Action<T> action)
-        {
-            if (node == null)
-            {
-                return;
-            }
-
-            this.EachInOrder(node.Left, action);
-            action(node.Value);
-            this.EachInOrder(node.Right, action);
+            if (startRange.CompareTo(node.Value) < 0)
+                this.Range(node.Left, queue, startRange, endRange);
+            if (startRange.CompareTo(node.Value) <= 0 &&
+                endRange.CompareTo(node.Value) >= 0)
+                queue.Enqueue(node.Value);
+            if (endRange.CompareTo(node.Value) > 0)
+                this.Range(node.Right, queue, startRange, endRange);           
         }
     }
 }
