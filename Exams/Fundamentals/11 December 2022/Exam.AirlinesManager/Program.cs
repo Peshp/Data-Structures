87dc4d5fa14949c9dc9ad99ea79cef0649fbe19d
Anton Todorov <98225837+Peshp@users.noreply.github.com>
2023-12-04 15:00:49 +0200

    Advanced Exam sceleton

---
+++ Exams/Fundamentals/11 December 2022/Exam.AirlinesManager/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Exam.DeliveriesManager;
+
+namespace Exam.AirlinesManager
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Airlines airline = new Airline(1, "a", 5);
+        }
+    }
+}
