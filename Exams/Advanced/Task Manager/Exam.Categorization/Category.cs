9dec5f0e85ef2be2b5ff1535c8e25a8a08a05936
Anton Todorov <98225837+Peshp@users.noreply.github.com>
2023-12-08 06:48:04 +0200

    Create Category.cs

---
+++ Exams/Advanced/Task Manager/Exam.Categorization/Category.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Exam.Categorization
+{
+    public class Category
+    {
+        public string Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public void AddChild(Category child)
+        {
+            if (Children.Any(c => c.Id == child.Id))
+                throw new ArgumentException("Child category already exists.");
+
+            child.Parent = this;
+            Children.Add(child);
+        }
+
+        public Category(string id, string name, string description)
+        {
+            Id = id;
+            Name = name;
+            Description = description;
+            Children = new List<Category>();
+        }
+    }
+}
